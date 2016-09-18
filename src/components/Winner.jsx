d1418eef1322943398ad765fac33e182c18766ba
Herbert Kagumba <herbert.kagumba@andela.com>
2016-09-18 23:03:00 +0300

    First working version

---
+++ src/components/Winner.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+class Winner extends Component {
+  constructor(props) {
+    super(props)
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+  }
+  render() {
+    return (
+      <div className='winner'>Winner is {this.props.winner}!</div>
+    )
+  }
+}
+
+export default Winner
